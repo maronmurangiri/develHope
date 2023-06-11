42198e896d36dcbe63972f9c341be4f5767c56c9
Maron <murangirimaron@gmail.com>
2023-06-12 02:04:51 +0300

    Added PrintYourNameAndSurname.java  file in co.develhope.introduction package

---
+++ co.develhope.introduction/PrintYourNameAndSurname.java
@@ -0,0 +1,8 @@
+package co.develhope.introduction;
+
+public class PrintYourNameAndSurname {
+    public static void main(String args[]){
+        System.out.print("Maron ");
+        System.out.print("Murangiri");
+    }
+}
