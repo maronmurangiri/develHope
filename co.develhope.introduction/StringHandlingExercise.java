25fddae4e679ed4baa4d537974f5683049f7cd1b
Maron <murangirimaron@gmail.com>
2023-06-12 03:48:54 +0300

    Added content in StringHandlingExercise.java

--- co.develhope.introduction/StringHandlingExercise.java
+++ co.develhope.introduction/StringHandlingExercise.java
@@ -1,2 +1,16 @@
-package co.develhope.introduction;public class StringHandlingExercise {
+package co.develhope.introduction;
+
+public class StringHandlingExercise {
+    public static void main(String args[]){
+
+        //declare and initialize string1 and string2
+        String string1 = "Hello";
+        String string2 = "How are you?";
+
+        //create charArray of characters and fill it with string1 first two characters and string2 last two characters
+        char[] charArray = {string1.charAt(0),string1.charAt(1),string2.charAt(string2.length()-2),string2.charAt(string2.length()-1)};
+
+        //print out the charArray
+        System.out.println(charArray);
+    }
 }
