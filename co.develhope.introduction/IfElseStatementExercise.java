a5ea7b18b3231b9ae3407a3d40aa7b5c1cf687cd
Maron <murangirimaron@gmail.com>
2023-06-12 03:59:48 +0300

    Added the file 'IfElseStatementExercise.java' to the package  co.develhope.introduction

--- co.develhope.introduction/IfElseStatementExercise.java
+++ co.develhope.introduction/IfElseStatementExercise.java
@@ -1,2 +1,17 @@
-package co.develhope.introduction;public class IfElseStatementExercise {
+package co.develhope.introduction;
+
+public class IfElseStatementExercise {
+    public static void main(String args[]){
+
+        //define and assigning myName variable
+        String myName = "Maron";
+
+        //Using if statement to chack and print if the number of letters in your name is even or odd
+        if(myName.length() %2 == 0){
+            System.out.println("The number of letters in the name : " + myName + " is even");
+        }
+        else {
+            System.out.println("The number of letters in the name : " + myName + " is odd");
+        }
+    }
 }
