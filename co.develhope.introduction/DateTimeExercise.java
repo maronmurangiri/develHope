5e6ff0a59557011d2505cd92d2755ac9d93f6016
Maron <murangirimaron@gmail.com>
2023-06-16 17:17:17 +0300

    Added the file 'DateTimeExercise.java' to package co.develhope.introduction

---
+++ co.develhope.introduction/DateTimeExercise.java
@@ -0,0 +1,20 @@
+package co.develhope.introduction;
+
+import java.time.LocalDateTime;
+import  java.time.LocalDate;
+import java.time.Month;
+import java.time.Year;
+import java.time.chrono.ChronoLocalDate;
+
+public class DateTimeExercise {
+    public static void main(String[] args){
+
+        //establishing and printing days left till the end of month
+        int daysLeftTillTheEndOfMonth = LocalDate.now().lengthOfMonth() - LocalDate.now().getDayOfMonth();
+        System.out.println(daysLeftTillTheEndOfMonth+ " days left till the end of the Month");
+
+        //establishing and printing days left till the end of the month
+        int daysLeftTillTheEndOfYear = LocalDate.now().lengthOfYear() - LocalDate.now().getDayOfYear();
+        System.out.println(daysLeftTillTheEndOfYear + " days left till the end of the year");
+    }
+}
