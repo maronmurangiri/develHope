ce095b1254ca51fcfa3a400597acaa87ef0b62ef
Maron <murangirimaron@gmail.com>
2023-06-16 13:30:47 +0300

    Added the file 'ForLoop.java' to package co.develhope.introduction

---
+++ co.develhope.introduction/ForLoopExercise.java
@@ -0,0 +1,9 @@
+package co.develhope.introduction;
+
+public class ForLoopExercise {
+    public static void main(String[] args){
+        for (int i = 5; i <=25;i++){
+            System.out.println(i);
+        }
+    }
+}
